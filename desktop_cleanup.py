57d263bb17baec1209225446f5da336afcb3895c
Ben Turner <benpturner1@gmail.com>
2024-03-07 16:06:56 +1100

    Added Others & Removed if not created

--- desktop_cleanup.py
+++ desktop_cleanup.py
@@ -3,6 +3,7 @@ import shutil
 import schedule
 import time
 import send2trash
+from datetime import datetime
 
 desktop_dir = os.path.join(os.path.expanduser('~'), 'Desktop')
 cleanup_folder = 'Cleanup'
@@ -14,39 +15,47 @@ others_dir = 'Others'
 path1 = os.path.join(cleanup_dir, screen_dir)
 path2 = os.path.join(cleanup_dir, images_dir)
 path3 = os.path.join(cleanup_dir, movies_dir)
+path4 = os.path.join(cleanup_dir, others_dir)
+now = datetime.now()
+current_time = now.strftime('%H:%M:%S')
 #Create a folder called 'cleanup' on my desktop
 #if cleanup doesn't exist, I need to create it
 #create 3 x subfolders in cleanup. 1 for screenshots 1 for images (including pdf, jpg etc), 1 for movie files
 def cleanup():
-     if not os.path.exists(cleanup_dir):
-        os.makedirs(cleanup_dir)
-        os.makedirs(path1)
-        os.makedirs(path2)
-        os.makedirs(path3)
-        print('Creating Cleanup Folder...')
+    os.makedirs(cleanup_dir)
+    os.makedirs(path1)
+    os.makedirs(path2)
+    os.makedirs(path3)
+    os.makedirs(path4)
+    print('Creating Cleanup Folder...')
 
      #Loop through all files on desktop   
-     imgs = ('pdf', 'jpg', 'HEIC')
-     movies = ('mp4', 'mov', 'MOV')
-     files_on_desktop = os.listdir(desktop_dir)
+    imgs = ('pdf', 'jpg', 'HEIC')
+    movies = ('mp4', 'mov', 'MOV')
+    others = ('zip', 'dmg' 'xls')
+    files_on_desktop = os.listdir(desktop_dir)
      # if there is a screenshot move it to screenshots folder
-     for file in files_on_desktop:
-         if file.startswith('Screenshot') or file.startswith('Screen Shot'):
-             file_dir = desktop_dir + '/' + file
-             shutil.move(file_dir, path1)
-             print(f'Moving {file}')
-     #if there is an image, move it to the images folder
-         elif file.endswith(imgs):
-             file_dir = desktop_dir + '/' + file
-             shutil.move(file_dir, path2)
-             print(f'Moving {file}')
-     # if there is a video, move to videos folder
-         elif file.endswith(movies):
-             file_dir = desktop_dir + '/' + file
-             shutil.move(file_dir, path3)
-             print(f'Moving {file}')
-             
-     print('Cleanup folder completed at 1002am')       
+    for file in files_on_desktop:
+        if file.startswith('Screenshot') or file.startswith('Screen Shot'):
+            file_dir = desktop_dir + '/' + file
+            shutil.move(file_dir, path1)
+            print(f'Moving {file}')
+    #if there is an image, move it to the images folder
+        elif file.endswith(imgs):
+            file_dir = desktop_dir + '/' + file
+            shutil.move(file_dir, path2)
+            print(f'Moving {file}')
+    # if there is a video, move to videos folder
+        elif file.endswith(movies):
+            file_dir = desktop_dir + '/' + file
+            shutil.move(file_dir, path3)
+            print(f'Moving {file}')
+        elif file.endswith(others):
+            file_dir = desktop_dir + '/' + file
+            shutil.move(file_dir, path4)
+            print(f'Moving {file}')
+            
+    print(f'Cleanup folder completed at: {current_time}')       
       
 #Schedule files in the screenshots folder to be deleted after 7 days
 def deletes():
@@ -56,14 +65,20 @@ def deletes():
                 path = os.path.join(folder, file)
                 print(f'{file} has been deleted')
                 send2trash.send2trash(path)
+                
+start = input('Would you like to cleanup your desktop? yes/no:')
+if start == 'yes':
+    cleanup()    
+else: 
+    print('Cleanup suspended')            
 
 #Schedule auto run for both deletes and cleanup
-schedule.every(1).monday.at("10:00").do(deletes)
-schedule.every(1).monday.at("10:02").do(cleanup)
+#schedule.every(1).monday.at("10:00").do(deletes)
+#schedule.every(1).monday.at("10:02").do(cleanup)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1) 
+#while True:
+    #schedule.run_pending()
+    #time.sleep(1) 
 
 
 

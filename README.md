58aca7679c1f956998577a543929930b209eedbc
Ben Turner <benpturner1@gmail.com>
2024-03-07 11:52:00 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+This is a basic desktop cleanup program that I have started as my first Python project
+It will be added to as I learn more of Python
+
+For now, its operation is to iterate over the users desktop and grab any screenshots, image files and movie files and compile those files into a parent folder called 'Cleanup' and sub folders of 'screenshots' 'images' and 'movies'
+
+It is scheduled to run once a week every Monday
+
+It is scheduled to send the screenshots folder to the recycling bin once a week every monday
---
+++ desktop_cleanup.py
@@ -0,0 +1,67 @@
+import os
+import shutil
+import schedule
+import time
+import send2trash
+
+desktop_dir = '/Users/benturner/Desktop'
+cleanup_dir = '/Users/benturner/Desktop/CleanUp'
+screen_dir = 'Screenshots'
+images_dir = 'Images'
+movies_dir = 'Videos'
+path1 = os.path.join(cleanup_dir, screen_dir)
+path2 = os.path.join(cleanup_dir, images_dir)
+path3 = os.path.join(cleanup_dir, movies_dir)
+#Create a folder called 'cleanup' on my desktop
+#if cleanup doesn't exist, I need to create it
+#create 3 x subfolders in cleanup. 1 for screenshots 1 for images (including pdf, jpg etc), 1 for movie files
+def cleanup():
+     if not os.path.exists(cleanup_dir):
+        os.makedirs(cleanup_dir)
+        os.makedirs(path1)
+        os.makedirs(path2)
+        os.makedirs(path3)
+        print('Creating Cleanup Folder...')
+
+     #Loop through all files on desktop   
+     imgs = ('pdf', 'jpg', 'jpeg')
+     movies = ('mp4', 'mov', 'MOV')
+     files_on_desktop = os.listdir(desktop_dir)
+     # if there is a screenshot move it to screenshots folder
+     for file in files_on_desktop:
+         if file.startswith('Screenshot') or file.startswith('Screen Shot'):
+             file_dir = desktop_dir + '/' + file
+             shutil.move(file_dir, path1)
+             print(f'Moving {file}')
+     #if there is an image, move it to the images folder
+         elif file.endswith(imgs):
+             file_dir = desktop_dir + '/' + file
+             shutil.move(file_dir, path2)
+             print(f'Moving {file}')
+     # if there is a video, move to videos folder
+         elif file.endswith(movies):
+             file_dir = desktop_dir + '/' + file
+             shutil.move(file_dir, path3)
+             print(f'Moving {file}')
+             
+     print('Cleanup folder completed at 1002am')       
+      
+#Schedule files in the screenshots folder to be deleted after 7 days
+def deletes():
+    for folder, subfolders, files in os.walk('/Users/benturner/Desktop/CleanUp/Screenshots'):
+        for file in files:
+            if file.startswith('Screenshot') or file.startswith('Screen Shot'):
+                path = os.path.join(folder, file)
+                print(f'{file} has been deleted')
+                send2trash.send2trash(path)
+
+#Schedule auto run for both deletes and cleanup
+schedule.every(1).monday.at("10:00").do(deletes)
+schedule.every(1).monday.at("10:02").do(cleanup)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1) 
+
+
+
